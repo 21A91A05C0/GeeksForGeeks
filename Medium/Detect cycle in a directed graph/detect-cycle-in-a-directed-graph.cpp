f6960ddaebffe05630b381003fe147d07b1eb41c
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2024-06-02 21:49:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -6,37 +6,63 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(vector<int> adj[],int i,vector<int>&vis,vector<int>&pathvis)
+    int dfs(int i,vector<int>&vis,vector<int>adj[],vector<int>&pathvis)
     {
         vis[i]=1;
         pathvis[i]=1;
         for(auto it:adj[i])
         {
-            //int k=adj[i][it];
-            if(!vis[it])
+            if(vis[it]==0)
             {
-                if(dfs(adj,it,vis,pathvis)==1) return 1;
+                if(dfs(it,vis,adj,pathvis)==1) return 1;
             }
-            else if(pathvis[it]) return 1;
+             else if(vis[it]==1 && pathvis[it]==1)
+            {
+                return 1;
+            }
             
             
         }
         pathvis[i]=0;
         return 0;
     }
-    bool isCyclic(int V, vector<int> adj[]) 
-    {
-        // code here
-        vector<int>vis(V+1,0);
-        vector<int>pathvis(V+1,0);
-        for(int i=0;i<V;i++)
-        {
-            if(vis[i]==0)
-            {
-                if(dfs(adj,i,vis,pathvis)==1) return 1;
-            }
-        }
-        return 0;
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int>ans;
+	    queue<int>pq;
+	    vector<int>indeg(V,0);
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it:adj[i])
+	        {
+	            indeg[it]++;
+	        }
+	    }
+	    for(int i=0;i<indeg.size();i++) 
+	    {
+	        if(indeg[i]==0)
+	        {
+	            pq.push(i);
+	            ans.push_back(i);
+	        }
+	    }
+	    while(!pq.empty())
+	    {
+	        int t=pq.front();
+	        pq.pop();
+	        for(auto it:adj[t])
+	        {
+	            indeg[it]--;
+	            if(indeg[it]==0)
+	            {
+	                pq.push(it);
+	                ans.push_back(it);
+	            }
+	        }
+	        
+	    }
+	    if(ans.size()!=V) return 1;
+	    return 0;
+        
     }
 };
 
