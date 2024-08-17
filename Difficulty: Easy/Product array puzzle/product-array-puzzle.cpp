6e1497060bb7c481b9e748c1753dd0a20eadcb79
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2024-08-17 09:50:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+
+        // code here
+        vector<long long int>ans(nums.size()+1,0);
+        long long int p=1;
+        int zero=0;
+        for(long long int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                zero++;
+            }
+            else
+            {
+                p=p*nums[i];
+            }
+        }
+        if(zero>=2) return ans;
+        else
+        {
+            for(long long int i=0;i<nums.size();i++)
+            {
+                if(nums[i]==0)
+                {
+                    ans[i]=p;
+                }
+                else
+                {
+                    if(zero!=0)
+                    {
+                        ans[i]=0;
+                    }
+                    else
+                    {
+                        ans[i]=p/nums[i];
+                    }
+                }
+            }
+            return ans;
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int n; // size of the array
+        cin >> n;
+        vector<long long int> arr(n), vec(n);
+
+        for (int i = 0; i < n; i++) // input the array
+        {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr); // function call
+
+        for (int i = 0; i < n; i++) // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
